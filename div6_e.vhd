eac6ff648028f20fdaa6a8a934f1a52d32422671
hallmar <38192440+hallmar@users.noreply.github.com>
2018-11-06 15:44:49 +0100

    Update and rename div4_e.vhd to div6_e.vhd

--- div6_e.vhd
+++ div6_e.vhd
@@ -1,11 +1,11 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity div4_e is
+entity div6_e is
 port(
 		en_i: IN std_logic;
 		clk_i: IN std_logic;
 		rb_i: IN std_logic;
 		c_o: OUT std_logic
 	);
-end div4_e;
+end div6_e;
