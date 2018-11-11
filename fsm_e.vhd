90f940350323a1d7173d6af7700113cff4e7a7bc
hallmar <38192440+hallmar@users.noreply.github.com>
2018-11-11 03:24:13 +0100

    Update fsm_e.vhd

--- fsm_e.vhd
+++ fsm_e.vhd
@@ -7,7 +7,6 @@ entity fsm_e is
     RX_i      : in     std_logic;
     clk_i     : in     std_logic;
     compare_o : out    std_logic;
-    nxt_o     : out    std_logic;
     rb_i      : in     std_logic;
     sren_o    : out    std_logic);
 end entity fsm_e;
